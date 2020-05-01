fc1618ff723225408868df5456363d7d458183cf
Janusz <janklyz@gmail.com>
2020-05-01 14:50:56 +0200

    final version project 1

--- Project1/DetailViewController.swift
+++ Project1/DetailViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class DetailViewController: UIViewController {
     
-
+    @IBOutlet var imageView: UIImageView!
+    
+    var selectedImage: String?
+    var selectedPictureNumber: Int?
+    var totalPictures: Int?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        guard let selectedImage = selectedImage else {
+            print( "no image selected")
+            return
+        }
+        if let value1 = selectedPictureNumber, let value2 = totalPictures{
+            title = "Picture \(value1) of \(value2)"
+        }
+        navigationItem.largeTitleDisplayMode = .never
+        imageView.image = UIImage(named: selectedImage)
+        
     }
-    
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+         super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
 }
